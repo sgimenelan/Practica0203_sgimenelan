f7dad9a829e3cb4bbc4ad668bb37ecf6a4a3145a
Fgoldarlar <sgimenelan@educacion.navarra.es>
2024-10-22 11:31:33 +0200

    Create Ejercicio10.py

---
+++ Ejercicio10.py
@@ -0,0 +1,8 @@
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
+#pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
+c1 = "Oxford"
+c2 = ""
+while (c2 != c1):
+    c2 = input("Dime la contraseña: ")
+    print("Error")
+print("OK")
