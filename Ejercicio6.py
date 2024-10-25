da17388883f512118d1f3cecfb1ad7b7326f458b
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-25 08:24:48 +0200

    Delete Ejercicio6.py

--- Ejercicio6.py
+++
@@ -1,5 +0,0 @@
-#Escribir un programa que pregunte al usuario su edad y 
-# muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
-edad = int(input("Dime tu edad: "))
-for i in range(1, edad + 1):
-    print (i)
