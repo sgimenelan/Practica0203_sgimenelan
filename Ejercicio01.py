66f84b436f00cb53c5ff1f2a2a064584e66d7165
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-25 08:19:55 +0200

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,6 @@
+contraseña = "oxford"
+contraseña_usuario = (input("Dime la contraseña: ")).lower()
+if contraseña == contraseña_usuario:
+    print("la contraseña es correcta")
+else: 
+    print("La contraseña es incorrecta")
