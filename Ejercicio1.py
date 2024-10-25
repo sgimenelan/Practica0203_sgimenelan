abf7e09d888cc7fd4883e116bc0f997b6d362043
sgimenelan <sgimenelan@educacion.navarra.es>
2024-10-25 08:19:59 +0200

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,6 +0,0 @@
-contraseña = "oxford"
-contraseña_usuario = (input("Dime la contraseña: ")).lower()
-if contraseña == contraseña_usuario:
-    print("la contraseña es correcta")
-else: 
-    print("La contraseña es incorrecta")
